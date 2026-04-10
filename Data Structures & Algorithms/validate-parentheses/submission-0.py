74e05a6e885304b4db22822114148e958de9c1d9
sanju49b <143645105+sanju49b@users.noreply.github.com>
2026-04-10 11:53:31 +0530

    Add: validate-parentheses - submission-0

---
+++ Data Structures & Algorithms/validate-parentheses/submission-0.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        hashmap = {")":"(","]":"[","}":"{"}
+        stack=[]
+        for c in s:
+            if c in hashmap.keys():
+                #if this is for closing bracket
+                if stack and stack[-1] == hashmap[c]:
+                    stack.pop()
+                else:
+                    return False
+            else:
+                stack.append(c)
+        return True if not stack else False
